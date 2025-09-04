6b6fd32a04823771a4949ecabe897d6e05ac15d4
Adam Turner <9087854+aa-turner@users.noreply.github.com>
2025-09-04 05:42:16 +0000

    Top PyPI sdists for Thu Sep  4 05:42:15 UTC 2025

--- extracted/dd/ddtrace/ddtrace/_version.py
+++ extracted/dd/ddtrace/ddtrace/_version.py
@@ -28,7 +28,7 @@ version_tuple: VERSION_TUPLE
 commit_id: COMMIT_ID
 __commit_id__: COMMIT_ID
 
-__version__ = version = '3.12.4'
-__version_tuple__ = version_tuple = (3, 12, 4)
+__version__ = version = '3.12.5'
+__version_tuple__ = version_tuple = (3, 12, 5)
 
-__commit_id__ = commit_id = 'gd1258014f'
+__commit_id__ = commit_id = 'g59627ec07'
