eca19c419081cdfcd1aefddf389c70d14a14234f
Adam Turner <9087854+aa-turner@users.noreply.github.com>
2026-02-18 05:53:59 +0000

    Top PyPI sdists for Wed Feb 18 05:53:58 UTC 2026

--- extracted/da/datadog-api-client/examples/v2/security-monitoring/CreateJiraIssues_379590688.py
+++ extracted/da/datadog-api-client/examples/v2/security-monitoring/CreateJiraIssues_379590688.py
@@ -4,24 +4,14 @@ Create Jira issue for security finding returns "Created" response
 
 from datadog_api_client import ApiClient, Configuration
 from datadog_api_client.v2.api.security_monitoring_api import SecurityMonitoringApi
-from datadog_api_client.v2.model.case_data_type import CaseDataType
 from datadog_api_client.v2.model.case_management_project import CaseManagementProject
 from datadog_api_client.v2.model.case_management_project_data import CaseManagementProjectData
 from datadog_api_client.v2.model.case_management_project_data_type import CaseManagementProjectDataType
-from datadog_api_client.v2.model.create_case_request_data import CreateCaseRequestData
-from datadog_api_client.v2.model.create_case_request_data_attributes import CreateCaseRequestDataAttributes
-from datadog_api_client.v2.model.create_case_request_data_relationships import CreateCaseRequestDataRelationships
 from datadog_api_client.v2.model.create_jira_issue_request_array import CreateJiraIssueRequestArray
 from datadog_api_client.v2.model.create_jira_issue_request_data import CreateJiraIssueRequestData
 from datadog_api_client.v2.model.create_jira_issue_request_data_attributes import CreateJiraIssueRequestDataAttributes
 from datadog_api_client.v2.model.create_jira_issue_request_data_relationships import (
     CreateJiraIssueRequestDataRelationships,
-)
-from datadog_api_client.v2.model.create_jira_issue_request_data_relationships_case import (
-    CreateJiraIssueRequestDataRelationshipsCase,
-)
-from datadog_api_client.v2.model.create_jira_issue_request_data_relationships_case_data import (
-    CreateJiraIssueRequestDataRelationshipsCaseData,
 )
 from datadog_api_client.v2.model.finding_data import FindingData
 from datadog_api_client.v2.model.finding_data_type import FindingDataType
@@ -31,50 +21,27 @@ from datadog_api_client.v2.model.jira_issues_data_type import JiraIssuesDataType
 body = CreateJiraIssueRequestArray(
     data=[
         CreateJiraIssueRequestData(
-            type=JiraIssuesDataType.JIRA_ISSUES,
-            attributes=CreateJiraIssueRequestDataAttributes(),
-            relationships=CreateJiraIssueRequestDataRelationships(
-                case=CreateJiraIssueRequestDataRelationshipsCase(
-                    data=CreateJiraIssueRequestDataRelationshipsCaseData(
-                        type=CaseDataType.CASES,
-                        id="6a773295-8729-4034-aada-53b64cbe02e7",
-                    ),
-                ),
-            ),
-        ),
-    ],
-    included=[
-        CreateCaseRequestData(
-            type=CaseDataType.CASES,
-            attributes=CreateCaseRequestDataAttributes(
+            attributes=CreateJiraIssueRequestDataAttributes(
                 title="A title",
                 description="A description",
             ),
-            relationships=CreateCaseRequestDataRelationships(
-                project=CaseManagementProject(
-                    data=CaseManagementProjectData(
-                        type=CaseManagementProjectDataType.PROJECTS,
-                        id="959a6f71-bac8-4027-b1d3-2264f569296f",
-                    ),
-                ),
+            relationships=CreateJiraIssueRequestDataRelationships(
                 findings=Findings(
                     data=[
                         FindingData(
+                            id="YmNlZmJhYTcyMDU5ZDk0ZDhiNjRmNGI0NDk4MDdiNzN-MDJlMjg0NzNmYzJiODY2MzJkNjU0OTI4NmVhZTUyY2U=",
                             type=FindingDataType.FINDINGS,
-                            id="ZGZhMDI3ZjdjMDM3YjJmNzcxNTlhZGMwMjdmZWNiNTZ-MTVlYTNmYWU3NjNlOTNlYTE2YjM4N2JmZmI4Yjk5N2Y=",
                         ),
                     ],
                 ),
+                project=CaseManagementProject(
+                    data=CaseManagementProjectData(
+                        id="959a6f71-bac8-4027-b1d3-2264f569296f",
+                        type=CaseManagementProjectDataType.PROJECTS,
+                    ),
+                ),
             ),
-            id="6a773295-8729-4034-aada-53b64cbe02e7",
-        ),
-        CaseManagementProjectData(
-            type=CaseManagementProjectDataType.PROJECTS,
-            id="959a6f71-bac8-4027-b1d3-2264f569296f",
-        ),
-        FindingData(
-            type=FindingDataType.FINDINGS,
-            id="ZGZhMDI3ZjdjMDM3YjJmNzcxNTlhZGMwMjdmZWNiNTZ-MTVlYTNmYWU3NjNlOTNlYTE2YjM4N2JmZmI4Yjk5N2Y=",
+            type=JiraIssuesDataType.JIRA_ISSUES,
         ),
     ],
 )
--- extracted/da/datadog-api-client/examples/v2/security-monitoring/CreateJiraIssues_829823123.py
+++ extracted/da/datadog-api-client/examples/v2/security-monitoring/CreateJiraIssues_829823123.py
@@ -4,24 +4,14 @@ Create Jira issue for security findings returns "Created" response
 
 from datadog_api_client import ApiClient, Configuration
 from datadog_api_client.v2.api.security_monitoring_api import SecurityMonitoringApi
-from datadog_api_client.v2.model.case_data_type import CaseDataType
 from datadog_api_client.v2.model.case_management_project import CaseManagementProject
 from datadog_api_client.v2.model.case_management_project_data import CaseManagementProjectData
 from datadog_api_client.v2.model.case_management_project_data_type import CaseManagementProjectDataType
-from datadog_api_client.v2.model.create_case_request_data import CreateCaseRequestData
-from datadog_api_client.v2.model.create_case_request_data_attributes import CreateCaseRequestDataAttributes
-from datadog_api_client.v2.model.create_case_request_data_relationships import CreateCaseRequestDataRelationships
 from datadog_api_client.v2.model.create_jira_issue_request_array import CreateJiraIssueRequestArray
 from datadog_api_client.v2.model.create_jira_issue_request_data import CreateJiraIssueRequestData
 from datadog_api_client.v2.model.create_jira_issue_request_data_attributes import CreateJiraIssueRequestDataAttributes
 from datadog_api_client.v2.model.create_jira_issue_request_data_relationships import (
     CreateJiraIssueRequestDataRelationships,
-)
-from datadog_api_client.v2.model.create_jira_issue_request_data_relationships_case import (
-    CreateJiraIssueRequestDataRelationshipsCase,
-)
-from datadog_api_client.v2.model.create_jira_issue_request_data_relationships_case_data import (
-    CreateJiraIssueRequestDataRelationshipsCaseData,
 )
 from datadog_api_client.v2.model.finding_data import FindingData
 from datadog_api_client.v2.model.finding_data_type import FindingDataType
@@ -31,58 +21,31 @@ from datadog_api_client.v2.model.jira_issues_data_type import JiraIssuesDataType
 body = CreateJiraIssueRequestArray(
     data=[
         CreateJiraIssueRequestData(
-            type=JiraIssuesDataType.JIRA_ISSUES,
-            attributes=CreateJiraIssueRequestDataAttributes(),
-            relationships=CreateJiraIssueRequestDataRelationships(
-                case=CreateJiraIssueRequestDataRelationshipsCase(
-                    data=CreateJiraIssueRequestDataRelationshipsCaseData(
-                        type=CaseDataType.CASES,
-                        id="e469ceda-957a-4557-a607-9ff25032e9ca",
-                    ),
-                ),
-            ),
-        ),
-    ],
-    included=[
-        CreateCaseRequestData(
-            type=CaseDataType.CASES,
-            attributes=CreateCaseRequestDataAttributes(
+            attributes=CreateJiraIssueRequestDataAttributes(
                 title="A title",
                 description="A description",
             ),
-            relationships=CreateCaseRequestDataRelationships(
-                project=CaseManagementProject(
-                    data=CaseManagementProjectData(
-                        type=CaseManagementProjectDataType.PROJECTS,
-                        id="959a6f71-bac8-4027-b1d3-2264f569296f",
-                    ),
-                ),
+            relationships=CreateJiraIssueRequestDataRelationships(
                 findings=Findings(
                     data=[
                         FindingData(
+                            id="a3ZoLXNjbS14eXV-aS0wNWY5MGYwMGE4NDg2ODdlOA==",
                             type=FindingDataType.FINDINGS,
-                            id="MzUxMDI4OWYyYWEyODRhYjQ0Zjg2YjY2ZTFmNjRjYzd-NDU2OWQyNTk1MjM5OGI2NzJjMTVhYjhiODY1ZDcwZWY=",
                         ),
                         FindingData(
+                            id="eWswLWJsdC1hZm5-aS0wMjRlYTgwMzVkZTU1MGIwYQ==",
                             type=FindingDataType.FINDINGS,
-                            id="ZjE2ZGI5YjdmYTQyYzhhMDQ3Nzc3YjM1NGQ2Y2NmZTd-NDU2OWQyNTk1MjM5OGI2NzJjMTVhYjhiODY1ZDcwZWY=",
                         ),
                     ],
                 ),
+                project=CaseManagementProject(
+                    data=CaseManagementProjectData(
+                        id="959a6f71-bac8-4027-b1d3-2264f569296f",
+                        type=CaseManagementProjectDataType.PROJECTS,
+                    ),
+                ),
             ),
-            id="e469ceda-957a-4557-a607-9ff25032e9ca",
-        ),
-        CaseManagementProjectData(
-            type=CaseManagementProjectDataType.PROJECTS,
-            id="959a6f71-bac8-4027-b1d3-2264f569296f",
-        ),
-        FindingData(
-            type=FindingDataType.FINDINGS,
-            id="MzUxMDI4OWYyYWEyODRhYjQ0Zjg2YjY2ZTFmNjRjYzd-NDU2OWQyNTk1MjM5OGI2NzJjMTVhYjhiODY1ZDcwZWY=",
-        ),
-        FindingData(
-            type=FindingDataType.FINDINGS,
-            id="ZjE2ZGI5YjdmYTQyYzhhMDQ3Nzc3YjM1NGQ2Y2NmZTd-NDU2OWQyNTk1MjM5OGI2NzJjMTVhYjhiODY1ZDcwZWY=",
+            type=JiraIssuesDataType.JIRA_ISSUES,
         ),
     ],
 )
