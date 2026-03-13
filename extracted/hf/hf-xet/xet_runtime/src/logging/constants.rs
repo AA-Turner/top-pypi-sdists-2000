19525b10ead866d1e5b160417c7e2b6228851c5a
Adam Turner <9087854+aa-turner@users.noreply.github.com>
2026-03-13 05:48:41 +0000

    Top PyPI sdists for Fri Mar 13 05:48:40 UTC 2026

---
+++ extracted/hf/hf-xet/xet_runtime/src/logging/constants.rs
@@ -0,0 +1,3 @@
+/// Default log level for the library to use. Override using the `RUST_LOG` env variable.
+pub(crate) const DEFAULT_LOG_LEVEL_FILE: &str = "info";
+pub(crate) const DEFAULT_LOG_LEVEL_CONSOLE: &str = "warn";
