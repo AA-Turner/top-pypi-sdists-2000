6f1e8dfd6e9566607996ec988a627608bdde9eaa
Adam Turner <9087854+aa-turner@users.noreply.github.com>
2026-09-05 05:36:36 +0000

    Top PyPI sdists for Sat Sep  5 05:36:35 UTC 2026

--- extracted/cl/cloudflare/src/cloudflare/types/accounts/subscription_create_params.py
+++ extracted/cl/cloudflare/src/cloudflare/types/accounts/subscription_create_params.py
@@ -2,7 +2,7 @@
 
 from __future__ import annotations
 
-from typing_extensions import Literal, Required, TypedDict
+from typing_extensions import Literal, TypedDict
 
 from ..shared_params.rate_plan import RatePlan
 
@@ -10,8 +10,11 @@ __all__ = ["SubscriptionCreateParams"]
 
 
 class SubscriptionCreateParams(TypedDict, total=False):
-    account_id: Required[str]
-    """Identifier"""
+    account_id: str
+    """The Account ID to use for this endpoint. Mutually exclusive with the Zone ID."""
+
+    zone_id: str
+    """The Zone ID to use for this endpoint. Mutually exclusive with the Account ID."""
 
     frequency: Literal["weekly", "monthly", "quarterly", "yearly"]
     """How often the subscription is renewed automatically."""
